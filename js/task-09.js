7c9cfb843c10b78584167f6f18780a9cb881f40f
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-25 20:23:44 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const colorTextEl = document.querySelector('.color');
+
+colorTextEl.textContent = '- #ffffff';
+
+
+btnEl.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorTextEl.textContent = `- ${color}`;
+}
