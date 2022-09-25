447a01478cae0c4924ebf8f3d2dfbd2d3826d0a6
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-25 22:08:36 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,77 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const inputEl = document.querySelector('[type="number"]');
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+
+const boxesEl = document.querySelector('#boxes');
+
+btnCreateEl.addEventListener('click', ()=> {
+  const widthBoxe = 30;
+  const heightBoxe = 30;
+
+  const boxes = [];
+
+  if (inputEl.value) {
+    for (let index = 0; index < inputEl.value; index++) {
+    const boxe = document.createElement('div')
+    boxe.style.width = `${widthBoxe + (10 * index)}px`;
+    boxe.style.height = `${heightBoxe + (10 * index)}px`;
+    boxe.style.backgroundColor = getRandomHexColor();
+
+    boxes.push(boxe);
+  }
+  } else {
+    return alert('sadadssa');
+  }
+  
+
+  console.log(boxes);
+  boxesEl.append(...boxes);
+
+});
+
+btnDestroyEl.addEventListener('click', () => {
+  [...boxesEl.children].map(child => child.remove())
+  inputEl.value = 0;
+});
+
+
+
+// btnCreateEl.addEventListener('click', createBoxes(inputEl.value))
+// btnDestroyEl.addEventListener('click', destroyBoxes());
+
+
+// function createBoxes(amount) {
+//   const widthBoxe = 30;
+//   const heightBoxe = 30;
+
+//   const boxes4 = [];
+
+//   if (amount) {
+//     for (let index = 0; index < amount; index++) {
+//     const boxe = document.createElement('div')
+//     boxe.style.width = widthBoxe + (10 * index);
+//     boxe.style.height = heightBoxe + (10 * index);
+//     boxe.style.backgroundColor = getRandomHexColor();
+
+//     boxes.push(boxe);
+//   }
+//   } else {
+//     return alert('sadadssa');
+//   }
+  
+
+//   console.log('boxes4');
+//   boxesEl.append(...boxes4);
+
+// }
+
+// function destroyBoxes() {
+//   [...boxesEl.children].map(child => child.remove());
+//  inputEl.value = 0;
+// }
+
