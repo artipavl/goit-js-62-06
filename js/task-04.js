2c700ce27f2e19ce29490d335a6e353c1a1e5f65
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-25 16:09:39 +0300

    Додає виконане 4ДЗ

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+const valueEl = document.querySelector('#value');
+const btnIncrement = document.querySelector('[data-action="increment"]');
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+
+let counterValue = Number(valueEl.textContent);
+
+btnIncrement.addEventListener('click', () => {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
+});
+
+
+btnDecrement.addEventListener('click', () => {
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+});
+
+
