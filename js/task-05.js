25814f792e982504af0fc4620dd8fec3c76e8733
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-25 17:19:25 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,19 @@
+const inputEl = document.querySelector('#name-input');
+const nameEl = document.querySelector('#name-output');
+
+
+inputEl.addEventListener('input', addName);
+
+// function addName(event) {
+//      if (event.currentTarget.value) {
+//         nameEl.textContent = event.currentTarget.value;
+//     } else {
+//         nameEl.textContent = 'Anonymous';
+//     } 
+// }
+
+function addName(event) {
+    event.currentTarget.value
+    ?nameEl.textContent = event.currentTarget.value
+    :nameEl.textContent = 'Anonymous';
+}
