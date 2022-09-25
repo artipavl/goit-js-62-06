0716ef4786bd4738fa23cd3f3a20d8c76db73083
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-25 14:25:00 +0300

    Додає виконання 2 ДЗ

--- js/task-02.js
+++ js/task-02.js
@@ -7,4 +7,20 @@ const ingredients = [
   'Condiments',
 ];
 
-const listEl = document.querySelector('ingredients')
+const listEl = document.querySelector('#ingredients');
+
+const addItemsInList = (list, items) => {
+  const itemsArr = [];
+
+  items.map(item => {
+    const itemEl = document.createElement('li')
+    itemEl.textContent = item;
+
+    return itemsArr.push(itemEl)
+  });
+
+  list.append(...itemsArr);
+}
+
+addItemsInList(listEl, ingredients);
+
