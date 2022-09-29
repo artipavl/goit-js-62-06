5a2643cc14754003347c5e38a78cd3bba307f575
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-29 10:57:08 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,15 +2,29 @@ const inputEl = document.querySelector("#validation-input");
 
 inputEl.addEventListener("blur", onInputBlur);
 
-function onInputBlur(event) {
-  const elEvent = event.currentTarget;
-  const dataLength = elEvent.dataset.length;
-  const elLength = elEvent.value.length;
-  if (elLength >= dataLength) {
-    elEvent.classList.remove("invalid");
-    elEvent.classList.add("valid");
-  } else {
-    elEvent.classList.remove("valid");
-    elEvent.classList.add("invalid");
+// function onInputBlur(event) {
+//   const elEvent = event.currentTarget;
+//   const dataLength = Number(elEvent.dataset.length);
+//   const elLength = elEvent.value.length;
+//   if (elLength === dataLength) {
+//     elEvent.classList.remove("invalid");
+//     elEvent.classList.add("valid");
+//   } else {
+//     elEvent.classList.remove("valid");
+//     elEvent.classList.add("invalid");
+//   }
+// }
+
+// варіант оптимізації
+
+function onInputBlur() {
+  const dataLength = Number(this.dataset.length);
+  const elLength = this.value.length;
+
+  this.classList.add("invalid");
+
+  if (elLength === dataLength) {
+    this.classList.remove("invalid");
+    this.classList.add("valid");
   }
 }
