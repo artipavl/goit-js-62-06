9c32695fd67f78794bbba7926d0e1003064b4498
artipavl <40296816+artipavl@users.noreply.github.com>
2022-09-29 11:32:17 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -10,57 +10,27 @@ const boxesEl = document.querySelector("#boxes");
 btnCreateEl.addEventListener("click", createBoxes);
 btnDestroyEl.addEventListener("click", destroyBoxes);
 
-function createBoxes(event, amount = inputEl.value) {
-  const widthBoxe = 30;
-  const heightBoxe = 30;
+function createBoxes() {
+  let sizeBox = 30;
+
   const boxes = [];
 
-  if (amount > 0) {
-    for (let index = 0; index < amount; index++) {
-      const boxe = document.createElement("div");
-      boxe.style.width = `${widthBoxe + 10 * index}px`;
-      boxe.style.height = `${heightBoxe + 10 * index}px`;
-      boxe.style.backgroundColor = getRandomHexColor();
+  if (inputEl.value) {
+    for (let index = 0; index < inputEl.value; index++) {
+      const boxe = `<div style="width: ${sizeBox}px; height: ${sizeBox}px; background-color: ${getRandomHexColor()};"></div>`;
 
       boxes.push(boxe);
+
+      sizeBox += 10;
     }
   } else {
     return alert("введіть число");
   }
 
-  boxesEl.append(...boxes);
+  boxesEl.insertAdjacentHTML("beforeend", boxes.join(""));
 }
 
 function destroyBoxes() {
-  [...boxesEl.children].map((child) => child.remove());
-  inputEl.value = 0;
+  boxesEl.innerHTML = "";
+  inputEl.value = "";
 }
-
-// btnCreateEl.addEventListener('click', ()=> {
-//   const widthBoxe = 30;
-//   const heightBoxe = 30;
-
-//   const boxes = [];
-
-//   if (inputEl.value) {
-//     for (let index = 0; index < inputEl.value; index++) {
-//     const boxe = document.createElement('div')
-//     boxe.style.width = `${widthBoxe + (10 * index)}px`;
-//     boxe.style.height = `${heightBoxe + (10 * index)}px`;
-//     boxe.style.backgroundColor = getRandomHexColor();
-
-//     boxes.push(boxe);
-//   }
-//   } else {
-//     return alert('sadadssa');
-//   }
-
-//   console.log(boxes);
-//   boxesEl.append(...boxes);
-
-// });
-
-// btnDestroyEl.addEventListener('click', () => {
-//   [...boxesEl.children].map(child => child.remove())
-//   inputEl.value = 0;
-// });
